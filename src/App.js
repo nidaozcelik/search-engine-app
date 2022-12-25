0553b5e8e9c47ed5fa781049b66b1a5e58d73db8
Nida Özçelik <nida.ozcelik@konzek.com>
2022-12-26 00:28:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from './components/search'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route, } from "react-router-dom";
+import Account from './components/search/account'
+import Navbar from './components/layout/navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='container'>
+      <div className='search-container'>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Search />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div >
+  )
 }
 
-export default App;
+export default App
