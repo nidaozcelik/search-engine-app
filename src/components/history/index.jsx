6aff63d59e7b8399bc54222a76e9fc6d02af9cae
Nida Özçelik <nida.ozcelik@konzek.com>
2023-01-11 19:43:36 +0300

    history component style is fixed

--- src/components/history/index.jsx
+++ src/components/history/index.jsx
@@ -2,24 +2,28 @@ import React from 'react'
 import HistoryWordMeaning from './history-word-meaning'
 
 const History = () => {
-  return <div className='history-accordion'>
+  return <div className=''>
     {
       Object.keys(localStorage).length === 0 ?
-       <span className='history-warning'>There is no data to show. Please search for word!</span>
-        : Object.entries(localStorage).map((key, value) => {
-          return <div key={value} className='history-accordion-item'>
-            <input type='checkbox' id={value} className='history-accordion-input' />
-            <label for={value} className='history-accordion-label'>
-              <span>{key[0].replace(/"/g, "").toUpperCase()}</span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
-              </svg>
-            </label>
-            <div class='history-accordion-content'>
-              <HistoryWordMeaning word={key[0].replace(/"/g, "")} />
-            </div>
-          </div>
-        })
+        <span className='history-warning'>There is no data to show. Please search for word!</span>
+        : <div className='history-accordion'>
+          {
+            Object.entries(localStorage).map((key, value) => {
+              return <div key={value} className='history-accordion-item'>
+                <input type='checkbox' id={value} className='history-accordion-input' />
+                <label for={value} className='history-accordion-label'>
+                  <span>{key[0].replace(/"/g, "").toUpperCase()}</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                  </svg>
+                </label>
+                <div class='history-accordion-content'>
+                  <HistoryWordMeaning word={key[0].replace(/"/g, "")} />
+                </div>
+              </div>
+            })
+          }
+        </div>
     }
   </div>
 }
